3ea6b1a2297d10272a3d514aa96d7dfc96a10ad5
lokesh0044 <lokeshsai0044@gmail.com>
2024-07-16 11:29:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  Navigate,
+} from 'react-router-dom';
+
+import Home from './components/Home/index.js';
+import NotFound from './components/NotFound/index.js';
+import Jobs from './components/Jobs/index.js';
+import JobItemDetails from './components/JobDetails/index.js';
+import LoginWrapper from './components/LoginWrapper/index.js';
+import ProtectedRoute from './components/ProtectedRoute/index.js';
+import './App.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route exact path="/login" element={<LoginWrapper />} />
+      <Route exact path="/not-found" element={<NotFound />} />
+      <Route
+        exact
+        path="/jobs"
+        element={
+          <ProtectedRoute>
+            <Jobs />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        exact
+        path="/jobs/:id"
+        element={
+          <ProtectedRoute>
+            <JobItemDetails useParams={useParams()} />
+          </ProtectedRoute>
+        }
+      />
+      <Navigate to="/not-found" />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
